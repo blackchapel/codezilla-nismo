1aab3fcfeeecafe6f0b721ed7e211818eb254e5b
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-02-15 16:57:56 +0530

    revised components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Dashboard from "./components/Dashboard";
+import ForgotPassword from "./components/ForgotPassword";
+import Login from "./components/Login";
+import MyProfile from "./components/MyProfile";
 import NavBar from "./components/NavBar";
+import OtpSignup from "./components/OtpSignup";
 import Signup from "./components/Signup";
 
 function App() {
@@ -12,6 +16,11 @@ function App() {
    <Routes>
      <Route path="/" element={<Dashboard />}/>
      <Route path="/signup" element={<Signup />} />
+     <Route path="/login" element={<Login />} />
+     <Route path="/forgotpassword" element={<ForgotPassword />} />
+     <Route path="/dashboard" element={<Dashboard />} />
+     <Route path="/otpverification" element={<OtpSignup />} />
+     <Route path="/myprofile" element={<MyProfile />} />
    </Routes>
    </BrowserRouter>
   );
---
+++ client/src/components/OtpSignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OtpSignup() {
+  return (
+    <div>OtpSignup</div>
+  )
+}
+
+export default OtpSignup
