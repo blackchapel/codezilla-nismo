cf3d481cff3fdd17b5dfc2a3f2222920cc715c82
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-02-16 22:12:09 +0530

    revised footer and dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { mainListItems, secondaryListItems } from './ListItems';
+import { Grow, Grid } from '@mui/material';
+import Card from '../layout/Card';
 
-function Dashboard() {
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function Dashboard() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+    <div>
+    <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ margin: 2, ...(open && { display: 'none' }), height: 1 }}
+          >
+            <MenuIcon />
+          </IconButton>
+    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+          height:"150px"
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {mainListItems}
+        </List>
+        <Divider />
+        <List>
+          {secondaryListItems}
+        </List>
+      </Drawer>
+      <Main open={open}>
+      <Grow
+        in={true}
+        style={{ transformOrigin: "0 0 0" }}
+        {...(2>1 ? { timeout: 2000 } : {})}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={{ xs: 2, md: 3 }}
+          padding={2}
+          sx={{
+            backgroundImage:
+            "url('https://wallpapercave.com/wp/wp7510787.jpg')",
+            marginLeft: "20px",
+          }}
+        >
+          <Grid item>
+            <Card />
+          </Grid>
+          <Grid item>
+            <Card />
+          </Grid>
+          <Grid item>
+            <Card />
+          </Grid>
+        </Grid>
+      </Grow>
+      </Main>
+    </Box>
+    </>
+  );
 }
-
-export default Dashboard
--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -1,132 +1,143 @@
 import React from "react";
-import { Grid, Typography, Paper, Button, ButtonGroup } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  ButtonGroup,
+  AppBar,
+} from "@mui/material";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Footer.css";
 function Footer() {
   return (
-    <Paper
-      elevation={7}
-      square={true}
-      sx={{
-        backgroundImage:
-          "url('https://media.istockphoto.com/vectors/abstract-two-tone-gradient-background-vector-id1197234186?b=1&k=20&m=1197234186&s=612x612&w=0&h=0bpI-k6I9DJ6wCuzI3OeLSTWwcc_5v7j8xVFo6GXIHQ=')",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <Grid 
-      conatainer
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="flex-start"
+    <footer>
+      <Paper
+        elevation={7}
+        square={true}
+        sx={{
+          backgroundImage:
+            "url('https://media.istockphoto.com/vectors/abstract-two-tone-gradient-background-vector-id1197234186?b=1&k=20&m=1197234186&s=612x612&w=0&h=0bpI-k6I9DJ6wCuzI3OeLSTWwcc_5v7j8xVFo6GXIHQ=')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          padding: "10px",
+        }}
       >
-        <Grid item style={{ margin: "auto" }}>
-          <Grid
+        <Grid
           container
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-          >
-            <Grid item style={{ fontSize: "50px" }}>
-              <FaGithub />
+          direction="row-reverse"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item>
+            <Grid
+              container
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Grid item style={{ fontSize: "50px" }}>
+                <FaGithub />
+              </Grid>
+              <Grid item>
+                <ButtonGroup variant="text">
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Kunal
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Yash
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Vidhita
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Rosita
+                    </a>
+                  </Button>
+                </ButtonGroup>
+              </Grid>
             </Grid>
-            <Grid item>
-            <ButtonGroup variant="text" >
-            <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                   Kunal 
-                  </a>
-              </Button>
-              <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Yash
-                  </a>
-                </Button>
-              <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Vidhita
-                  </a>
-                </Button>
-                <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                  Rosita
-                  </a>
-                </Button>
-            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Grid item style={{ fontSize: "50px" }}>
+                <FaLinkedin />
+              </Grid>
+              <Grid item>
+                <ButtonGroup variant="text">
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Kunal
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Yash
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Vidhita
+                    </a>
+                  </Button>
+                  <Button>
+                    <a
+                      href="https://github.com/Yashpb1513"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Rosita
+                    </a>
+                  </Button>
+                </ButtonGroup>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item style={{ marginTop: "20px" }}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-          >
-            <Grid item style={{ fontSize: "40px" }}>
-              <FaLinkedin />
-            </Grid>
-            <Grid item>
-            <ButtonGroup variant="text" >
-            <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                   Kunal 
-                  </a>
-              </Button>
-              <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Yash
-                  </a>
-                </Button>
-              <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Vidhita
-                  </a>
-                </Button>
-                <Button>
-                  <a
-                    href="https://github.com/Yashpb1513"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                  Rosita
-                  </a>
-                </Button>
-            </ButtonGroup>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </footer>
   );
 }
 
