aa750a825b570a3d160bb1d2f6d462c95769d356
Rosita <esorthe7@gmail.com>
2022-02-19 07:04:11 +0530

    Lots of things done

--- client/src/layout/Card.jsx
+++ client/src/layout/Card.jsx
@@ -13,16 +13,72 @@ import {
 import SendIcon from "@mui/icons-material/Send";
 import { CardContent, Typography } from "@mui/material";
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function Card() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [open, setOpen] = React.useState(false);
   const [placement, setPlacement] = React.useState();
-
+  const [orderAmount, setOrderAmount] = React.useState("");
+  const navigate = useNavigate();
+  
   const handleClick = (newPlacement) => (event) => {
     setAnchorEl(event.currentTarget);
     setOpen((prev) => placement !== newPlacement || !prev);
     setPlacement(newPlacement);
   };
+  const apiUrl = "http://localhost:5001/api/payments";
+  function loadRazorpay() {
+        const script = document.createElement('script');
+        script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+        script.onerror = () => {
+          alert('Razorpay SDK failed to load. Are you online?');
+        };
+        script.onload = async () => {
+          try {
+            
+            const result = await axios.post(apiUrl + '/create-order', {
+              amount: orderAmount + '00',
+            });
+            const { amount, id: order_id, currency } = result.data;
+            console.log(amount);
+            const {
+              data: { key: razorpayKey },
+            } = await axios.get(apiUrl + '/get-razorpay-key');
+    
+            const options = {
+              key: razorpayKey,
+              amount: amount.toString(),
+              currency: currency,
+              name: 'Avni Clan',
+              description: 'Donate any amount towards a greener future',
+              order_id: order_id,
+              handler: async function (response) {
+                const result = await axios.post(apiUrl + '/pay-order', {
+                  amount: amount,
+                  razorpayPaymentId: response.razorpay_payment_id,
+                  razorpayOrderId: response.razorpay_order_id,
+                  razorpaySignature: response.razorpay_signature,
+                });
+                alert(result.data.message);
+               
+              },
+              theme: {
+                color: '#8fd071',
+              },
+            };
+    
+         
+            const paymentObject = new window.Razorpay(options);
+            paymentObject.open();
+          } catch (err) {
+            alert(err);
+            
+          }
+        };
+        document.body.appendChild(script);
+      }
   return (
     <Cards
       sx={{
@@ -85,11 +141,15 @@ function Card() {
                             id="outlined-basic"
                             label="Amount"
                             variant="outlined"
+                            type="number"
+                            value={orderAmount}
+                            onChange={(e) => setOrderAmount(e.target.value)}
                           />
+                       
                         </Grid>
                         <Grid item>
                       
-                          <Button variant="contained" color="secondary">
+                          <Button variant="contained" color="secondary" onClick={() => {loadRazorpay();}}> 
                             Enter
                           </Button>
                         </Grid>
@@ -109,11 +169,13 @@ function Card() {
               </Button>
             </Grid>
             <Grid item>
+              
               <Button
                 variant="contained"
                 endIcon={<SendIcon />}
                 color="secondary"
                 sx={{height: "50px", width: "200px"}}
+                onClick={() => navigate("/signup", {replace:true})}
               >
                 <Typography variant="h5">Sign up</Typography>
               </Button>
